1c9308c591b3cb705e99bb61a7d995142bca3f85
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-10-14 13:19:46 +0600

    KeySimulator

---
+++ AppKeySimulator/AppKeySimulator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Threading;
+
+namespace GtaAppKeySimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Test();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Test()
+        {
+            while (true)
+            {
+                Thread.Sleep(5000);
+                NativeMethods.AppClick("Калькулятор", "1");
+            }
+           
+        }
+    }
+}
